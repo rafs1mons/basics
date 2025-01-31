754427c13e7cb00deb02e3c61134bf8e57686224
Ahmet Akkaya <aakkaya300@gmail.com>
2025-01-31 23:48:18 +0300

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers
+{
+    public class CourseController : Controller
+    {
+        public string Index()
+        {
+            return "course/index";
+        }
+
+        public string List()
+        {
+            return "course/list";
+        }
+    }
+}
