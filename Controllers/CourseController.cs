fee02836e13b8152d2b5b176cb88f1ed3aaae36c
Ahmet Akkaya <aakkaya300@gmail.com>
2025-02-02 23:40:37 +0300

    Details page and repository class

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -5,36 +5,19 @@ namespace basics.Controllers
 {
     public class CourseController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Details(int? id)
         {
-            var kurs = new Course();
-            kurs.Id = 1;
-            kurs.Title = "Aspnet Core Kursu";
-            kurs.Description = "Gucel bir kurs";
-            kurs.Image = "course1.jpg";
-            return View(kurs);
-        }
-
-        public IActionResult Details()
-        {
-            var kurs = new Course();
-            kurs.Id = 1;
-            kurs.Title = "Aspnet Core Kursu";
-            kurs.Description = "Gucel bir kurs";
-            kurs.Image = "course1.jpg";
+            if (id == null)
+            {
+                return RedirectToAction("List", "Course");
+            }
+            var kurs = Repository.GetById(id);
             return View(kurs);
         }
 
         public IActionResult List()
         {
-            var kurslar = new List<Course>()
-            {
-                new Course() { Id = 1, Title ="aspnet kursu", Description =" guzel kurs",Image ="course1.jpg"},
-                new Course() { Id = 2, Title ="php kursu", Description =" guzel kurs",Image ="course2.jpg"},
-                new Course() { Id = 3, Title ="java kursu", Description =" guzel kurs",Image ="course3.jpeg"}
-            };
-
-            return View("CourseList", kurslar);
+            return View("CourseList", Repository.Courses);
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿namespace basics.Models
+{
+    public class Repository
+    {
+        private static readonly List<Course> _courses = new();
+
+        static Repository()
+        {
+            _courses = new List<Course>()
+            {
+                new Course() { Id = 1, Title ="aspnet kursu", Description =" guzel kurs",Image ="course1.jpg"},
+                new Course() { Id = 2, Title ="php kursu", Description =" guzel kurs",Image ="course2.jpg"},
+                new Course() { Id = 3, Title ="java kursu", Description =" guzel kurs",Image ="course3.jpeg"}
+            };
+        }
+        public static List<Course> Courses { get { return _courses; } }
+
+        public static Course? GetById(int? id)
+        {
+            return _courses.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
